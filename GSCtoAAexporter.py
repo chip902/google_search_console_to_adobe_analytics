653b46fc3d1cac135561762fcd60ef09de3ded9a
Andrew Chepurny <andrew@chepurny.com>
2026-01-27 22:09:49 -0500

    Update .gitignore patterns, add date range support, improve GSC pagination, and expand documentation
    - Support explicit start_date/end_date config in addition to lookback_days
    - Implement pagination for GSC API queries with configurable max_rows_per_day limit
    - Make type_evar optional in config
    - Dynamically build datasource_columns based on configured eVars
    - Switch from data to json parameter in requests for proper typing

--- GSCtoAAexporter.py
+++ GSCtoAAexporter.py
@@ -15,54 +15,70 @@ from googleapiclient.errors import HttpError
 with open('config.json', 'r') as file:
     config = json.load(file)
 
-base = datetime.datetime.today()
-date_list = [(base - datetime.timedelta(days=x)).strftime("%Y-%m-%d") for x in range(config["lookback_days"])]
+if config.get("start_date") and config.get("end_date"):
+    start = datetime.datetime.strptime(config["start_date"], "%Y-%m-%d")
+    end = datetime.datetime.strptime(config["end_date"], "%Y-%m-%d")
+    delta = (end - start).days
+    date_list = [(start + datetime.timedelta(days=x)).strftime("%Y-%m-%d")
+                 for x in range(delta + 1)]
+    print("Using date range: {} to {}".format(
+        config["start_date"], config["end_date"]))
+else:
+    base = datetime.datetime.today()
+    date_list = [(base - datetime.timedelta(days=x)).strftime("%Y-%m-%d")
+                 for x in range(config["lookback_days"])]
+    print("Using lookback: {} days from today".format(config["lookback_days"]))
 
-if config["clicks_event"] or config["impressions_event"] or config["position_event"] or config["ctr_event"]:
-    if config["url_evar"] and config["keyword_evar"]:
+if config.get("clicks_event") or config.get("impressions_event") or config.get("position_event") or config.get("ctr_event"):
+    if config.get("url_evar") and config.get("keyword_evar"):
         print("Both URL and Keyword eVar given. Importing Keywords per URL...")
         operating_mode = "URL and Keyword"
-        query_dimensions = ['date','page','query']
-        datasource_columns = ['Date', 'Evar '+config["type_evar"], 'Evar '+config["url_evar"], 'Evar '+config["keyword_evar"]]
-    elif config["keyword_evar"]:
+        query_dimensions = ['date', 'page', 'query']
+        datasource_columns = ['Date']
+    elif config.get("keyword_evar"):
         print("Only Keyword eVar given. Importing only Keywords...")
         operating_mode = "Keyword Only"
-        query_dimensions = ['date','query']
-        datasource_columns = ['Date', 'Evar '+config["type_evar"], 'Evar '+config["keyword_evar"]]
-    elif config["url_evar"]:
+        query_dimensions = ['date', 'query']
+        datasource_columns = ['Date']
+    elif config.get("url_evar"):
         print("Only URL eVar given. Importing only URLs...")
         operating_mode = "URL Only"
-        query_dimensions = ['date','page']
-        datasource_columns = ['Date', 'Evar '+config["type_evar"], 'Evar '+config["url_evar"]]
+        query_dimensions = ['date', 'page']
+        datasource_columns = ['Date']
     else:
         print("No eVars given. Importing metrics only")
         operating_mode = "Metrics Only"
         query_dimensions = ['date']
-        datasource_columns = ['Date', 'Evar '+config["type_evar"]]
-    if config["clicks_event"]:
+        datasource_columns = ['Date']
+    if config.get("type_evar"):
+        datasource_columns.append('Evar '+config["type_evar"])
+    if config.get("url_evar") and operating_mode in ("URL and Keyword", "URL Only"):
+        datasource_columns.append('Evar '+config["url_evar"])
+    if config.get("keyword_evar") and operating_mode in ("URL and Keyword", "Keyword Only"):
+        datasource_columns.append('Evar '+config["keyword_evar"])
+    if config.get("clicks_event"):
         datasource_columns.append("Event "+config["clicks_event"])
-    if config["impressions_event"]:
+    if config.get("impressions_event"):
         datasource_columns.append("Event "+config["impressions_event"])
-    if config["position_event"]:
+    if config.get("position_event"):
         datasource_columns.append("Event "+config["position_event"])
-    if config["ctr_event"]:
+    if config.get("ctr_event"):
         datasource_columns.append("Event "+config["ctr_event"])
 else:
     print("No events given. Aborting...")
     sys.exit()
 
-if not config["type_evar"]:
-    print("No Type Evar given. Aborting...")
-    sys.exit()
 
 GOOGLE_SCOPES = ["https://www.googleapis.com/auth/webmasters.readonly"]
 GOOGLE_TOKEN_FILE = "google_token.json"
-GOOGLE_CLIENT_SECRETS_FILE = "client_secrets.json"
+GOOGLE_CLIENT_SECRETS_FILE = "client_secret_amtrak.com.json"
+
 
 def get_authenticated_google_service():
     creds = None
     if os.path.exists(GOOGLE_TOKEN_FILE):
-        creds = Credentials.from_authorized_user_file(GOOGLE_TOKEN_FILE, GOOGLE_SCOPES)
+        creds = Credentials.from_authorized_user_file(
+            GOOGLE_TOKEN_FILE, GOOGLE_SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -70,15 +86,17 @@ def get_authenticated_google_service():
             flow = InstalledAppFlow.from_client_secrets_file(
                 GOOGLE_CLIENT_SECRETS_FILE, GOOGLE_SCOPES
             )
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_local_server(port=8085)
         with open(GOOGLE_TOKEN_FILE, "w") as token:
             token.write(creds.to_json())
     return build("webmasters", "v3", credentials=creds)
+
 
 search_console = get_authenticated_google_service()
 
 ADOBE_TOKEN_URL = "https://ims-na1.adobelogin.com/ims/token/v3"
 ADOBE_DISCOVERY_URL = "https://analytics.adobe.io/discovery/me"
+
 
 def get_adobe_access_token(config):
     response = requests.post(
@@ -93,6 +111,7 @@ def get_adobe_access_token(config):
     response.raise_for_status()
     return response.json()["access_token"]
 
+
 def get_first_global_company_id(config, access_token):
     response = requests.get(
         ADOBE_DISCOVERY_URL,
@@ -104,23 +123,27 @@ def get_first_global_company_id(config, access_token):
     response.raise_for_status()
     return response.json().get("imsOrgs")[0].get("companies")[0].get("globalCompanyId")
 
+
 access_token = get_adobe_access_token(config)
 global_company_id = get_first_global_company_id(config, access_token)
 
 dataSources = requests.post(
-        "https://api.omniture.com/admin/1.4/rest/?method=DataSources.Get",
-        headers={
-            "Authorization": "Bearer {}".format(access_token),
-            "x-api-key": config["apiKey"],
-            "x-proxy-global-company-id": global_company_id
-        }, 
-        data={'reportSuiteID':config["report_suite_id"]}
-    ).json()
+    "https://api.omniture.com/admin/1.4/rest/?method=DataSources.Get",
+    headers={
+        "Authorization": "Bearer {}".format(access_token),
+        "x-api-key": config["apiKey"],
+        "x-proxy-global-company-id": global_company_id
+    },
+    json={'reportSuiteID': config["report_suite_id"]}
+)
+dataSources.raise_for_status()
+dataSources = dataSources.json()
 
+dataSourceID = None
 for dataSource in dataSources:
     if dataSource["name"] == config["data_source_name"]:
         dataSourceID = dataSource["id"]
-        print("Found Data Source ID")
+        print("Found Data Source ID: " + str(dataSourceID))
         break
 
 if dataSourceID:
@@ -130,13 +153,15 @@ if dataSourceID:
             "Authorization": "Bearer {}".format(access_token),
             "x-api-key": config["apiKey"],
             "x-proxy-global-company-id": global_company_id
-        }, 
-        data={'reportSuiteID':config["report_suite_id"],'dataSourceID':dataSourceID}
+        },
+        json={'reportSuiteID': config["report_suite_id"],
+              'dataSourceID': dataSourceID}
     ).json()
     for job in jobs:
         jobname = job["fileName"]
         if config["job_prefix"].lower() in jobname:
-            matchstring = '^'+re.escape(config["job_prefix"].lower())+"_"+operating_mode.lower()+'_([0-9]{4}-[0-9]{2}-[0-9]{2})_'+config["report_suite_id"]+'_'+dataSourceID+'_[0-9]*\.tab$'
+            matchstring = '^'+re.escape(config["job_prefix"].lower())+"_"+operating_mode.lower(
+            )+'_([0-9]{4}-[0-9]{2}-[0-9]{2})_'+config["report_suite_id"]+'_'+dataSourceID+r'_[0-9]*\.tab$'
             p = re.compile(matchstring)
             regex_match = p.match(job["fileName"])
             if regex_match and job["status"] != "failed":
@@ -150,54 +175,73 @@ else:
 print("Number of days to fetch: "+str(len(date_list)))
 i = 1
 for query_date in date_list:
-    print("Fetching Google Search Console Data for "+query_date+". Query "+str(i)+"/"+str(len(date_list)))
-    request = {
-        'startDate': query_date,
-        'endDate': query_date,
-        'dimensions': query_dimensions,
-        'rowLimit': 10000
-    }
+    print("Fetching Google Search Console Data for " +
+          query_date+". Query "+str(i)+"/"+str(len(date_list)))
+    max_rows_per_day = config.get("max_rows_per_day", 50000)
+    gsc_rows = []
+    start_row = 0
+    while True:
+        request = {
+            'startDate': query_date,
+            'endDate': query_date,
+            'dimensions': query_dimensions,
+            'rowLimit': 10000,
+            'startRow': start_row
+        }
+        result = search_console.searchanalytics().query(
+            siteUrl=config["google_property"], body=request).execute()
+        batch = result.get("rows", [])
+        gsc_rows.extend(batch)
+        print("  Fetched {} rows (total: {})".format(len(batch), len(gsc_rows)))
+        if len(batch) < 10000 or len(gsc_rows) >= max_rows_per_day:
+            break
+        start_row += len(batch)
+    if len(gsc_rows) >= max_rows_per_day:
+        gsc_rows = gsc_rows[:max_rows_per_day]
+        print("  Capped at {} rows (max_rows_per_day)".format(max_rows_per_day))
     result_rows = []
-    result = search_console.searchanalytics().query(siteUrl=config["google_property"], body=request).execute()
-    if "rows" in result:
-        print("Received "+str(len(result["rows"]))+" rows of data. Uploading to Adobe...")
-        for row in result["rows"]:
+    if gsc_rows:
+        print("Received "+str(len(gsc_rows)) +
+              " rows of data. Uploading to Adobe...")
+        for row in gsc_rows:
             row_to_append = []
-            row_to_append.append(row["keys"][0][5:7]+"/"+row["keys"][0][8:10]+"/"+row["keys"][0][0:4]+"/00/00/00")
-            row_to_append.append("Import Type: "+operating_mode)
+            row_to_append.append(
+                row["keys"][0][5:7]+"/"+row["keys"][0][8:10]+"/"+row["keys"][0][0:4]+"/00/00/00")
+            if config.get("type_evar"):
+                row_to_append.append("Import Type: "+operating_mode)
 
             if operating_mode != "Metrics Only":
                 row_to_append.append(row["keys"][1])
             if operating_mode == "URL and Keyword":
                 row_to_append.append(row["keys"][2])
 
-            if config["clicks_event"]:
+            if config.get("clicks_event"):
                 row_to_append.append(str(row["clicks"]))
-            if config["impressions_event"]:
+            if config.get("impressions_event"):
                 row_to_append.append(str(row["impressions"]))
-            if config["position_event"]:
+            if config.get("position_event"):
                 row_to_append.append(str(row["position"]))
-            if config["ctr_event"]:
+            if config.get("ctr_event"):
                 row_to_append.append(str(row["ctr"]))
             result_rows.append(row_to_append)
 
-        if len(result_rows)  > 0:
-            jobresponse = requests.post(
-                "https://api.omniture.com/admin/1.4/rest/?method=DataSources.UploadData",
-                headers={
-                    "Authorization": "Bearer {}".format(access_token),
-                    "x-api-key": config["apiKey"],
-                    "x-proxy-global-company-id": global_company_id
-                }, 
-                json={
-                    "columns": datasource_columns,
-                    'reportSuiteID': config["report_suite_id"],
-                    'dataSourceID':dataSourceID,
-                    "finished": True,
-                    "jobName": config["job_prefix"]+"_"+operating_mode+"_"+query_date,
-                    "rows": result_rows
-                }
-            )
+    if len(result_rows) > 0:
+        jobresponse = requests.post(
+            "https://api.omniture.com/admin/1.4/rest/?method=DataSources.UploadData",
+            headers={
+                "Authorization": "Bearer {}".format(access_token),
+                "x-api-key": config["apiKey"],
+                "x-proxy-global-company-id": global_company_id
+            },
+            json={
+                "columns": datasource_columns,
+                'reportSuiteID': config["report_suite_id"],
+                'dataSourceID': dataSourceID,
+                "finished": True,
+                "jobName": config["job_prefix"]+"_"+operating_mode+"_"+query_date,
+                "rows": result_rows
+            }
+        )
     else:
         print("No Data for "+query_date)
-    i+=1
+    i += 1
--- README.md
+++ README.md
@@ -1,11 +1,35 @@
 # google_search_console_to_adobe_analytics
-Small script to import Google Search performance data to Adobe Analytics via the 1.4 DataSources API.
 
-For usage instructions see https://www.fullstackanalyst.io/blog/adobe-analytics/importing-organic-google-search-data-to-adobe-analytics-with-a-single-script/?r=g
+Import natural search keywords and organic search performance data from Google Search Console into Adobe Analytics using the 1.4 DataSources API.
 
-Configuration has moved to a dedicated config file since initial release, thank you @chip902!
+Since Google encrypts search queries, Adobe Analytics cannot natively capture organic keyword data. This script bridges that gap by pulling keyword-level performance data from the Google Search Console API and uploading it to Adobe Analytics via Data Sources, making it available for reporting alongside your other Analytics dimensions.
 
-Initially created by Frederik Werner (https://www.frederikwerner.de/?r=g)
+For background, see the original article: [Importing Organic Google Search Data to Adobe Analytics with a Single Script](https://www.fullstackanalyst.io/blog/adobe-analytics/importing-organic-google-search-data-to-adobe-analytics-with-a-single-script/)
+
+Initially created by Frederik Werner (https://www.frederikwerner.de/?r=g). Configuration moved to a dedicated config file since initial release, thank you @chip902!
+
+## What It Does
+
+The script fetches the following from Google Search Console for each day in a configurable date range:
+
+- **Search queries (keywords)** — the natural search terms users typed
+- **Landing page URLs** — the pages that ranked for those terms
+- **Clicks, impressions, CTR, and average position** — performance metrics per keyword/URL
+
+It then uploads this data to Adobe Analytics via the Data Sources API, mapping keywords and URLs to eVars and metrics to events. The script tracks previously uploaded jobs to avoid duplicate imports.
+
+### Operating Modes
+
+The script automatically selects an import mode based on which eVars you configure:
+
+| Mode | Dimensions Imported | Required Config |
+|------|---------------------|-----------------|
+| **URL and Keyword** | Keywords per URL (most granular) | `url_evar` + `keyword_evar` |
+| **Keyword Only** | Search queries only | `keyword_evar` only |
+| **URL Only** | Landing pages only | `url_evar` only |
+| **Metrics Only** | Aggregate clicks/impressions/position/CTR | No eVars (events only) |
+
+Each mode also imports whichever metric events you configure (clicks, impressions, position, CTR). The `type_evar` is optional — if set, each row is labeled with the operating mode (e.g., "Import Type: URL and Keyword").
 
 ## Setup
 
@@ -14,6 +38,7 @@ Initially created by Frederik Werner (https://www.frederikwerner.de/?r=g)
 - Python 3.8+
 - A Google Cloud project with the Search Console API enabled and OAuth 2.0 credentials (`client_secrets.json`)
 - An Adobe Developer Console project with OAuth Server-to-Server credentials
+- A Data Source already created in Adobe Analytics (the script looks it up by name)
 
 ### Installation
 
@@ -23,9 +48,18 @@ pip install -r requirements.txt
 
 ### Google Authentication
 
-1. Create OAuth 2.0 credentials in the [Google Cloud Console](https://console.cloud.google.com/apis/credentials) (Desktop application type).
-2. Download the credentials JSON and save it as `client_secrets.json` in the project root.
-3. On first run, a browser window will open for you to authorize access to Search Console. The resulting token is saved to `google_token.json` for subsequent runs.
+1. Enable the **Google Search Console API** for your project (APIs & Services > Library > search for "Google Search Console API" > Enable).
+2. Configure the **OAuth consent screen** (APIs & Services > OAuth consent screen):
+   - Choose **Internal** (if using Google Workspace) or **External**.
+   - Fill in the required fields (app name, support email).
+   - Add the scope `https://www.googleapis.com/auth/webmasters.readonly`.
+   - If your app is in **Testing** status, you must add yourself as a test user under **Test users** — otherwise you'll get an `Error 403: access_denied` when trying to sign in. Alternatively, click **Publish App** to remove this restriction.
+3. Create OAuth 2.0 credentials in the [Credentials page](https://console.cloud.google.com/apis/credentials):
+   - Click **Create Credentials > OAuth client ID**
+   - Application type: **Desktop app** (not "Web application" — Desktop credentials automatically allow localhost redirects, avoiding `Error 400: redirect_uri_mismatch`)
+   - Give it a name (e.g., "GSC Exporter")
+4. Download the credentials JSON and save it as `client_secrets.json` in the project root.
+5. On first run, a browser window will open for you to authorize access to Search Console. The resulting token is saved to `google_token.json` for subsequent runs.
 
 > **Note:** If upgrading from the previous `oauth2client`-based version, you will need to re-authenticate on first run. The old `oauth2.json` token file is no longer used.
 
@@ -35,6 +69,38 @@ This script uses Adobe's **OAuth Server-to-Server** credentials (replacing the d
 
 1. In the [Adobe Developer Console](https://developer.adobe.com/console), create a project with OAuth Server-to-Server credentials and add the Adobe Analytics API.
 2. Copy the **Client ID**, **Client Secret**, and **Scopes** into `config.json`.
+
+### Adobe Analytics Setup
+
+Before running the script, set up the following in Adobe Analytics:
+
+#### 1. Create a Data Source
+
+1. Go to **Admin > Data Sources** in your report suite.
+2. Create a new Data Source and note the name — it must match `data_source_name` in your config.
+
+#### 2. Configure eVars
+
+Go to **Admin > Report Suites > Edit Settings > Conversion > Conversion Variables** and allocate eVars:
+
+| eVar | Purpose | Settings |
+|------|---------|----------|
+| **URL eVar** | Landing page URL from GSC. Use an existing Full URL eVar if you have one — GSC URLs won't have query strings, so they'll match your organic visits. | Allocation: Most Recent, Expiration: Hit |
+| **Keyword eVar** | Natural search keyword/query | Allocation: Most Recent, Expiration: Hit |
+| **Type eVar** (optional) | Labels rows with the import mode (e.g., "Import Type: URL and Keyword") | Allocation: Most Recent, Expiration: Hit |
+
+#### 3. Configure Events
+
+Go to **Admin > Report Suites > Edit Settings > Conversion > Success Events** and create four events:
+
+| Event | Purpose | Type | Polarity |
+|-------|---------|------|----------|
+| **GSC Clicks** | Click-throughs from search results | **Numeric** | Up is Good |
+| **GSC Impressions** | Times a result appeared in search | **Numeric** | Up is Good |
+| **GSC Avg Position** | Average ranking position | **Numeric** | Down is Good |
+| **GSC CTR** | Click-through rate (decimal, e.g., 0.053 = 5.3%) | **Numeric** | Up is Good |
+
+All four events must be **Numeric**, not Counter. The script passes specific values (e.g., 45 clicks, 1200 impressions) — a Counter event would just record "1" per row regardless of the actual value. Position polarity is "Down is Good" because a lower number means a higher ranking.
 
 ### Configuration
 
@@ -51,15 +117,30 @@ Copy `config.json` and fill in your values:
     "report_suite_id": "orgreportsuiteid",
     "job_prefix": "GSC-Import",
     "lookback_days": 100,
-    "type_evar": "197",
-    "url_evar": "198",
-    "keyword_evar": "199",
-    "ctr_event": "997",
+    "url_evar": "8",
+    "keyword_evar": "29",
     "clicks_event": "998",
     "impressions_event": "999",
-    "position_event": "1000"
+    "position_event": "1000",
+    "ctr_event": "997"
 }
 ```
+
+#### Date Range
+
+You can control which dates the script fetches using one of two approaches:
+
+- **Lookback days** (default): Set `lookback_days` to fetch the last N days from today.
+- **Explicit date range**: Set `start_date` and `end_date` (format: `YYYY-MM-DD`) to fetch a specific range. This is useful for testing with a single day or backfilling a specific period.
+
+```json
+"start_date": "2025-01-15",
+"end_date": "2025-01-15"
+```
+
+If both `start_date`/`end_date` and `lookback_days` are present, the explicit date range takes priority.
+
+#### Config Reference
 
 | Field | Description |
 |-------|-------------|
@@ -68,12 +149,20 @@ Copy `config.json` and fill in your values:
 | `orgId` | Adobe Organization ID |
 | `scopes` | Adobe OAuth scopes (comma-separated) |
 | `google_property` | Google Search Console property URL |
-| `data_source_name` | Name of the Adobe Analytics Data Source |
+| `data_source_name` | Name of the Data Source in Adobe Analytics (must match exactly) |
 | `report_suite_id` | Adobe Analytics Report Suite ID |
 | `job_prefix` | Prefix for upload job names (used for deduplication) |
-| `lookback_days` | Number of days to look back for data |
-| `type_evar` / `url_evar` / `keyword_evar` | eVar numbers for import type, URL, and keyword |
-| `clicks_event` / `impressions_event` / `position_event` / `ctr_event` | Event numbers for metrics |
+| `lookback_days` | Number of days to look back from today |
+| `start_date` | Start of explicit date range (`YYYY-MM-DD`), used instead of `lookback_days` |
+| `end_date` | End of explicit date range (`YYYY-MM-DD`), used instead of `lookback_days` |
+| `max_rows_per_day` | Maximum rows to fetch per day from GSC (optional, default: 50,000). Acts as a guardrail to prevent runaway queries. |
+| `type_evar` | eVar number to label the import type (optional) |
+| `url_evar` | eVar number for landing page URLs (optional — omit to skip URL import) |
+| `keyword_evar` | eVar number for natural search keywords (optional — omit to skip keyword import) |
+| `clicks_event` | Event number for clicks (Numeric) |
+| `impressions_event` | Event number for impressions (Numeric) |
+| `position_event` | Event number for average position (Numeric) |
+| `ctr_event` | Event number for click-through rate (Numeric) |
 
 ## Usage
 
@@ -81,4 +170,27 @@ Copy `config.json` and fill in your values:
 python GSCtoAAexporter.py
 ```
 
-The script will fetch Search Console data for each day in the lookback window (skipping dates already uploaded) and upload it to Adobe Analytics via the DataSources API.
+The script will:
+
+1. Authenticate with Google Search Console and Adobe Analytics
+2. Look up your Data Source by name and retrieve existing jobs
+3. Skip dates that have already been successfully uploaded
+4. For each remaining date, query GSC for up to 10,000 rows of keyword/URL performance data
+5. Upload each day's data to Adobe Analytics via the DataSources.UploadData API
+
+### Testing with a Single Day
+
+To test before doing a full import, set `start_date` and `end_date` to the same date:
+
+```json
+"start_date": "2025-01-20",
+"end_date": "2025-01-20"
+```
+
+This will fetch and upload just one day of data so you can verify everything is configured correctly.
+
+## Limitations
+
+- The GSC API returns up to 10,000 rows per request. The script automatically paginates to fetch all available data, capped at `max_rows_per_day` (default: 50,000) to prevent runaway queries.
+- GSC data is typically available with a 2-3 day delay
+- The script uses the Adobe Analytics 1.4 DataSources API (there is no 2.0 equivalent for aggregate data import)
